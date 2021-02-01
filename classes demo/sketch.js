ee455b4f154a9f43c2550d39fd47a92c0024b2d0
KyranMW <KyranMW@users.noreply.github.com>
2021-02-01 13:01:41 -0600

    classes and walkers demos

--- classes demo/sketch.js
+++ classes demo/sketch.js
@@ -1,15 +1,49 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Classes demo, OOP walker
 
+let theWalkers = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
-  background(220);
+  for (let i=0; i<theWalkers.length; i++){
+    theWalkers[i].move();
+    theWalkers[i].display();
+  }
+}
+
+function mousePressed(){
+  let someWalker = new Walker(mouseX, mouseY, "yellow");
+  theWalkers.push(someWalker);
+}
+
+class Walker {
+  constructor(x,y,theColor) {
+    this.x = x;
+    this.y = y;
+    this.color = theColor;
+    this.speed = 5;
+  }
+  display() {
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, 3, 3);
+  }
+  move() {
+    let choice = random(100);
+    if (choice < 25) {
+      this.x += this.speed;
+    }
+    else if (choice < 50) {
+      this.x -= this .speed;
+    }
+    else if (choice < 75) {
+      this.y += this .speed;
+    }
+    else if (choice < 100) {
+      this.y -= this .speed;
+    }
+  }
 }
