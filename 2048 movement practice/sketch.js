59d92b6c834c49c157dcce4f3e6317911dca744c
KyranMW <KyranMW@users.noreply.github.com>
2021-02-11 14:04:57 -0600

    created a practice thing to figure out movement for 2048

---
+++ 2048 movement practice/sketch.js
@@ -0,0 +1,22 @@
+let rows;
+let cols;
+let cellWidth;
+let cellHeight;
+let gridPattern, rightArrow, downArrow, leftArrow, upArrow;
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+  background(220);
+  translate()
+}
+
+class num{
+  constructor(){
+
+  }
+}
+
+function drawGrid
